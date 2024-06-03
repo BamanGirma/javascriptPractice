5a295c73c277f3389525d9193ec74103ed2577bc
unknown <ET@PCHQFIN030.ethiopianairlines.com>
2024-06-03 20:05:10 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// // declearing function
+// function printName() {
+//   return "Hi there";
+// }
+// printName();
+
+// var a = function printName() {
+//   return "Hi there";
+// };
+
+// var printName = () => {
+//   return "Hi there";
+// };
+
+// var printName = () => "Hi there";
+
+// // arrow function with one argument
+// const printName = a => "Hi there";
+
+var pass = 50;
+var score = 76;
+
+if (score >= pass) {
+  console.log("passed");
+}
+if (score < pass) console.log("failed");
+
+function checkIfPaased(name, score, pass) {
+  if (score >= pass) {
+    console.log(name + ": passed");
+  } else console.log(name + ": failed");
+}
+checkIfPaased("Aman", 80, 50);
+checkIfPaased("Girma", 30, 50);
+checkIfPaased("Gebreyes", 20, 50);
+
+var pass = 50;
+var score = 76;
+
+let result = score >= pass ? "passsed" : "failed";
+console.log(result);
+
+a=5;
+console.log(typeof(a));
