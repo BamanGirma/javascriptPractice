924f2b2d10ccaa7b70a4c1798b4e1fdc9c461d81
BamanGirma <amanuelgirma360@gmail.com>
2024-06-06 16:27:51 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -51,37 +51,58 @@ function add(a, b) {
 var sum = add(5, 10);
 console.log(sum);
 
-//
+//question seven
 function incrementOne(a) {
-  if (isNaN(a) == true){
-      return "this is string, please enter an integer";
-  } 
-   a+=1; 
-   return a;
+  if (isNaN(a) == true) {
+    return "this is string, please enter an integer";
+  }
+  a += 1;
+  return a;
 }
 var res = incrementOne(2);
 console.log(res);
 
-// 
-function tringleArea(b,h){
-    if (isNaN(b) || isNaN(h)) {
-      return "this is string, please enter an integer";
-    }
-    let area = (b*h)/2;
-    return area;
+//question eight
+function tringleArea(b, h) {
+  if (isNaN(b) || isNaN(h)) {
+    return "this is string, please enter an integer";
+  }
+  let area = (b * h) / 2;
+  return area;
 }
-    let calTriangleArea = tringleArea(10, 2);
-    console.log(calTriangleArea);
-    // 
-    function animal(chickens,cows,pigs){
-      if(isNaN(chickens) && isNaN(cows) && isNaN(pigs)){
-        return "this is a string, please enter an integer"
-      }
-      let chickensLeg = 2* chickens;
-      let cowsLegs= 4* cows;
-      let pigsLegs=4*pigs;
-      let totalLegs = chickensLeg + cowsLegs + pigsLegs;
-      return totalLegs;
-    }
-    let totalResults= animal(2,3,5);
-    console.log(totalResults);
+let calTriangleArea = tringleArea(10, 2);
+console.log(calTriangleArea);
+// question nine
+function animal(chickens, cows, pigs) {
+  if (isNaN(chickens) && isNaN(cows) && isNaN(pigs)) {
+    return "this is a string, please enter an integer";
+  }
+  let chickensLeg = 2 * chickens;
+  let cowsLegs = 4 * cows;
+  let pigsLegs = 4 * pigs;
+  let totalLegs = chickensLeg + cowsLegs + pigsLegs;
+  return totalLegs;
+}
+let totalResults = animal(2, 3, 5);
+console.log(totalResults);
+
+//question ten
+function threeTimesFirstElement(arr) {
+  if (arr.length !== 2) {
+    return "Array must contain exactly two elements";
+  }
+  return 3 * arr[0];
+}
+const results = threeTimesFirstElement([4, 5]);
+console.log(results); // Outputs: 12
+// question eleven
+function isSameNum(num1, num2) {
+  var res="";
+  var res = (num1 === num2) ? "true" : "false";
+  return res;
+}
+let ans = isSameNum("2", 2);
+console.log(ans);
+
+//question 12
+
